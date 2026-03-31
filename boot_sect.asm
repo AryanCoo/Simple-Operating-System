b59851a27480cb98e027ebf742c2edd52261aa3a
Aryan <jaryan329@gmail.com>
2026-03-31 21:13:48 +0530

    Files updated.

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,22 +1,25 @@
+[BITS 16]
+[ORG 0x7C00]
+
 ;
-; A simple boot sector program that loops forever.
+; A simple boot sector program that prints "Hello"
 ;
 
-mov ah, 0x0e ;
+mov ah, 0x0e    ; BIOS function: print character in teletype mode
 
 mov al, 'H'
-int 0x0e
+int 0x10
 mov al, 'e'
-int 0x0e
+int 0x10
 mov al, 'l'
-int 0x0e
+int 0x10
 mov al, 'l'
-int 0x0e
+int 0x10
 mov al, 'o'
-int 0x0e
+int 0x10
 
-jmp $ ;
+jmp $           ; Infinite loop
 
-times 510 -( $ - $$ ) db 0 ;
+times 510 - ($ - $$) db 0   ; Pad to 510 bytes
 
-dw 0xaa55 ;
+dw 0xaa55       ; Boot sector magic number
